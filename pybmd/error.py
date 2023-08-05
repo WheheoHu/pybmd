94598a86ba27214cc51acf9327830f8d9a590541
GitHub Action <action@github.com>
2023-08-05 07:19:51 +0000

    Update documentation

--- pybmd/error.py
+++
@@ -1,5 +0,0 @@
-class ResolveInitError(Exception):
-    """Error for Resolve Init
-    """
-    def __init__(self, msg="Init Resolve failed,please check if davinci resolve is running"):
-        super().__init__(msg)
--- pybmd/fusion_comp.py
+++
@@ -1,8 +0,0 @@
-class FusionComp():
-    """Fusion Composite Object"""
-    def __init__(self, fusion_comp):
-        self.fusion_comp = fusion_comp
-        
-    
-
-    
