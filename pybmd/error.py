e8d3a230367324894ec4ffbe28bfe4a402b7f6b3
WheheoHu <wheheohu@outlook.com>
2023-08-05 14:21:55 +0800

    test build

--- pybmd/error.py
+++ pybmd/error.py
@@ -1,3 +1,5 @@
 class ResolveInitError(Exception):
+    """Error for Resolve Init
+    """
     def __init__(self, msg="Init Resolve failed,please check if davinci resolve is running"):
         super().__init__(msg)
