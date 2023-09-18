03c33c8662bcf43c739fe59e2e0a4a91ae7a4748
WheheoHu <wheheohu@outlook.com>
2023-09-19 04:51:37 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import sys, os
-sys.path.append(os.path.abspath(".."))
+sys.path.insert(0,os.path.abspath(".."))
 
 import pybmd as package
 
--- docs/index.rst
+++ docs/index.rst
@@ -42,6 +42,7 @@ Samples
 
 .. autosummary::
    :toctree: _autosummary
+   :template: custom-module-template.rst
    :recursive:
 
    pybmd
