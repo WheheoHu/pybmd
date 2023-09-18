fbac1f472ce30353e20cb7ba6df1c3d80331cb30
WheheoHu <wheheohu@outlook.com>
2023-09-18 20:58:36 +0800

    update theme

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.napoleon',
               'sphinx.ext.viewcode',
               'sphinx_pdj_theme']
-
+html_theme = "pydata_sphinx_theme"
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
