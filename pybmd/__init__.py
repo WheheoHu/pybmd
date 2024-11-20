3cec7f9f470515ada120596ecf82535dbc7d8a25
WheheoHu <wheheohu@outlook.com>
2024-11-20 15:34:32 +0800

    init 2024.4.0

--- pybmd/__init__.py
+++ pybmd/__init__.py
@@ -4,8 +4,8 @@ from .ui_dispather import UI_Dispather
 __name__ = 'pybmd'
 
 __major_version__ = "2024"
-__minor_version__ = "3"
-__change_version__ = "4"
+__minor_version__ = "4"
+__change_version__ = "0"
 __version__ = ".".join(
     (__major_version__, __minor_version__, __change_version__)
 )
