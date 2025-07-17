21de8bd3c678e0b86eb25ecd400b7b2d6bec9597
WheheoHu <wheheohu@outlook.com>
2025-07-17 19:20:05 +0800

    clean up project

--- pybmd/__init__.py
+++ pybmd/__init__.py
@@ -3,10 +3,10 @@ from .ui_dispather import UI_Dispather
 
 __name__ = 'pybmd'
 
-__major_version__ = "2025"
-__minor_version__ = "2"
-__change_version__ = "0"
-__version__ = ".".join(
-    (__major_version__, __minor_version__, __change_version__)
-)
+# __major_version__ = "2025"
+# __minor_version__ = "2"
+# __change_version__ = "0"
+# __version__ = ".".join(
+#     (__major_version__, __minor_version__, __change_version__)
+# )
 # __version__="2025.2.0"
